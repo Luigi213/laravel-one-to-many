b6d48a7a4c6cd4032174a1d8421a78bc02a51715
Luigi213 <reyesgianluigi88@gmail.com>
2023-03-03 14:27:17 +0100

    set type with rcms and added migrate seeder; table

---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Models\Type;
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $types = ['laravel', 'phpplain', 'js', 'vue'];
+
+        foreach($types as $type){
+            $newType = new Type();
+    
+            $newType->name = $type;
+            $newType->save();
+        }
+    }
+}
+
